9acad0dae6990b5d5ef006817e372af75c9d5a9d
Arun Singh <120382062+Arunsingh9140@users.noreply.github.com>
2023-05-14 19:15:53 +0530

    C program run time float variable project
    Like & share

---
+++ c program run time float variable .c
@@ -0,0 +1,25 @@
+//program to display the name of item when price of item is enterd at run time
+
+#include<stdio.h>
+int main(){
+    float price;
+    
+    //Note:-Items availabe are:
+    //(1)Guitar at price $1000.60,(2)viloine at price $500.25,(3)Flute at price $200
+    
+    printf("Items availabe in price => $1000.60,$500.25,$200");
+    printf("\n select the price");
+    scanf("%f",&price);
+    if(price == 1000.60)
+    printf("\n Giutar");
+    else
+    if(price == 500.25)
+    printf("\nViolin");
+    else
+    if(price == 200)
+    printf("\n Flute");
+    else
+    printf("\n Item not avilable with this price");
+    return 0;
+    
+}
